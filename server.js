869f25f0c78fb804428a7c5b464f65729eb8c84d
Tomashiwa <wongqinliang@gmail.com>
2021-09-30 10:34:32 +0800

    Setup React

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const users = require("./routes/api/users");
 app.use(express.json());
 app.use("/api/users", users);
 
-app.listen("3000", () => {
-    console.log("Server started on port 3000...");
+app.listen("5000", () => {
+    console.log("Server started on port 5000...");
 })
