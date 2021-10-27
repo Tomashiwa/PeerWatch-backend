73f74b07b61a1dafcd5ff977b89547eb29ed2df7
Qin Liang <wongqinliang@gmail.com>
2021-10-27 20:33:38 +0800

    Change DB connection from singular connection to pool connections (#77)
    * Refactor db to use connection pool instead of singular connection
    
    * Remove some comments

--- services/db.js
+++ services/db.js
@@ -7,20 +7,55 @@ if (env_var.error) {
 	console.log("Failed to retrieve DB info stored as environment variables");
 }
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
-	database: "peerwatch", // Need this in order to work
+	database: "peerwatch",
+	connectionLimit: 75,
 });
 
-db.connect((err) => {
-	if (err) {
-		console.log(`DB error: ${err.message}`);
-	} else {
-		console.log("DB connected...");
-	}
+pool.on("connection", (connection) => {
+	console.log(`New connection made, ${connection.threadId}`);
 });
 
-module.exports = db;
+pool.on("acquire", (connection) => {
+	console.log(`Connection ${connection.threadId} acquired from pool`);
+});
+
+pool.on("release", (connection) => {
+	console.log(`Connection ${connection.threadId} released back to pool`);
+});
+
+module.exports = {
+	query: function () {
+		var sql_args = [];
+		var args = [];
+		for (var i = 0; i < arguments.length; i++) {
+			args.push(arguments[i]);
+		}
+		var callback = args[args.length - 1];
+
+		pool.getConnection(function (err, connection) {
+			if (err) {
+				console.log(err);
+				return callback(err);
+			}
+			if (args.length > 2) {
+				sql_args = args[1];
+			}
+
+			connection.query(args[0], sql_args, function (err, results) {
+				// Always put connection back in pool after last query
+				connection.release();
+
+				if (err) {
+					console.log(err);
+					return callback(err);
+				}
+				callback(null, results);
+			});
+		});
+	},
+};
