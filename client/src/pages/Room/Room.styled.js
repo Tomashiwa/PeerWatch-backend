c6ba63af251e4a3c907c7ed815fff2dc5eca999d
Qin Liang <wongqinliang@gmail.com>
2021-11-01 00:31:40 +0800

    Convert room page from flexboxes to grids (#99)
    * Convert landing from flex to grid
    
    * Convert room page from flex to grid

--- client/src/pages/Room/Room.styled.js
+++ client/src/pages/Room/Room.styled.js
@@ -5,22 +5,26 @@ export const RoomPageWrapper = styled.div`
 	min-height: 100%;
 	height: auto;
 
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+	// display: flex;
+	// flex-direction: column;
+	// justify-content: center;
+	// align-items: center;
+
+	display: grid;
+	grid-template-columns: auto;
+	place-items: center;
 
 	@media (max-width: 1000px) {
 		height: 100%;
-		justify-content: center;
+		justify-content: stretch;
 		align-items: center;
 	}
 `;
 
 export const RoomContainerWrapper = styled.div`
 	width: 98%;
-	height: 75%;
-	max-height: 75%;
+	// height: 75%;
+	// max-height: 75%;
 
 	display: grid;
 	grid-template-rows: none;
