28181615a063c4b419c73fb2cb44613a3248f641
Tomashiwa <wongqinliang@gmail.com>
2021-11-02 09:34:30 +0800

    Update URLs

--- client/src/util/url.js
+++ client/src/util/url.js
@@ -1,7 +1,7 @@
 const URL = {
 	LOCAL_SERVER_URL: "http://localhost:5000",
-	DEPLOYED_SERVER_URL: "http://54.179.111.98:5000",
-	FALLBACK_VIDEO: "https://www.youtube.com/watch?v=Ski_KEgOUP4",
+	DEPLOYED_SERVER_URL: "http://13.250.235.202:5000",
+	FALLBACK_VIDEO: "https://www.youtube.com/watch?v=pdCaVAPZLXM",
 };
 
 export default URL;
