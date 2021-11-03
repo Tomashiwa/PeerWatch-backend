732a50283cf041c36f8cbfd3d0a49e4748f8fe3d
Tomashiwa <wongqinliang@gmail.com>
2021-11-03 22:40:54 +0800

    Update redis with deployment ip

--- services/redis.js
+++ services/redis.js
@@ -1,7 +1,8 @@
 const { createAdapter } = require("@socket.io/redis-adapter");
 const { createClient } = require("redis");
 
-const pubClient = createClient({ host: "localhost", port: 6379 });
+const host = process.env.NODE_ENV === "production" ? "54.179.111.98" : "localhost";
+const pubClient = createClient({ host, port: 6379 });
 const subClient = pubClient.duplicate();
 const redisClient = pubClient.duplicate();
 
