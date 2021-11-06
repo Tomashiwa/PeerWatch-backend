60cf674576c1d8a4afe25e581c42bef7f70677a0
Tomashiwa <wongqinliang@gmail.com>
2021-11-06 11:58:49 +0800

    Update SQL script to set canChat and canVideo to 1

--- sql/peerwatch_users_in_rooms.sql
+++ sql/peerwatch_users_in_rooms.sql
@@ -35,8 +35,8 @@ DROP TABLE IF EXISTS `users_in_rooms`;
 CREATE TABLE `users_in_rooms` (
   `userId` int NOT NULL,
   `roomId` varchar(255) NOT NULL,
-  `canChat` tinyint(1) NOT NULL DEFAULT '0',
-  `canVideo` tinyint(1) NOT NULL DEFAULT '0',
+  `canChat` tinyint(1) NOT NULL DEFAULT '1',
+  `canVideo` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`userId`,`roomId`) USING BTREE,
   KEY `fk_users_in_rooms_rooms` (`roomId`) USING BTREE,
   CONSTRAINT `fk_users_in_rooms_rooms` FOREIGN KEY (`roomId`) REFERENCES `rooms` (`roomId`) ON DELETE CASCADE ON UPDATE CASCADE,
